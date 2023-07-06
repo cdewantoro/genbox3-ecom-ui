a08544098efb11fbcf7dc1ee2d07df77b84f03ca
Yunus Dermawan <yunus.shevcenko@gmail.com>
2023-07-06 23:17:05 +0700

    adding login and register

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
-    <div class="login">
-      Login
+    <div class="container d-flex flex-column align-items-center">
+      <div class="login container d-flex justify-content-center align-items-center">
+        <form>
+          <div class="mb-1">
+            <input type="text" class="form-control border-0" id="username" required placeholder="Masukkan username">
+          </div>
+          <hr>
+          <div class="mb-1">
+            <input type="password" class="form-control border-0" id="password" required placeholder="Masukkan password">
+          </div>
+          <button type="submit" class="btn btn-primary w-100 mt-3">Login</button>
+        </form>
+      </div>
+      <div class="mt-5">
+        <span class="me-2">Belum punya akun?</span>
+        <router-link to="/register" class="fw-bold">Daftar</router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -14,10 +29,20 @@ export default {
 
 <style scoped>
 .login {
-  width: 600px; 
+  width: 400px; 
   height: 400px; 
   background-color: white; 
   border-radius: 15px;
   box-shadow: 0px 4px 24px 0px rgba(0, 4, 45, 0.15);
+  overflow: hidden;
+}
+hr {
+  height: 2px;;
+}
+input {
+  text-align: center;
+}
+input::placeholder {
+  text-align: center;
 }
 </style>
